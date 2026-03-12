6fbedff86088e50c350fe1c253bf1a3747e61e2f
Dallas Quin <166860264+dallyq@users.noreply.github.com>
2026-03-12 18:52:51 +0200

    Added comments and code for the first two project requirements.

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,20 @@
-const fruits = ["apple", "banana", "grapes"];
+// Add needed variables
+const qwerty = document.getElementById("qwerty");
+const phrase = document.getElementById("phrase");
+const btnReset = document.getElementsByClassName("btn-reset");
+// Double check that 'missed' is correct
+const missed = 0;
 
-console.log(fruits);
+// Create an array named 'phrases'
+// Declare and initialize the phrases array, storing at least
+// five strings that contain only letters & spaces = no 
+// punctuation
+
+const phrases = [
+    'Joey',
+    'Pheobe',
+    'Chandler',
+    'Monica',
+    'Rachel'
+];
+
